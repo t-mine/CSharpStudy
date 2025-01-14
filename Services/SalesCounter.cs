deded02162f0d09b383774533a08348c77980fcf
t-mine <tomonori.mine.biz@gmail.com>
2025-01-14 22:19:44 +0900

    Chapter2

---
+++ Services/SalesCounter.cs
@@ -0,0 +1,18 @@
+﻿using CSharpStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.Services;
+
+internal class SalesCounter
+{
+    private readonly List<Sale> _sales;
+
+    public SalesCounter(List<Sale> sales)
+    {
+        _sales = sales;
+    }
+}
