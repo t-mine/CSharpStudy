d772d19cde8d749c11c5619a54e1c2dd23d6e5ec
t-mine <tomonori.mine.biz@gmail.com>
2025-01-21 21:16:23 +0900

    Chapter3

--- Chapters/Chapter3.cs
+++ Chapters/Chapter3.cs
@@ -28,5 +28,67 @@ public static class Chapter3
         var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         var count = Count(numbers, x => x > 5);
         Console.WriteLine(count);
+
+        // 問題3.1
+        var nums = new List<int> { 12, 87, 94, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+        // 問題3.1-1
+        Console.WriteLine("問題3.1-1");
+        var ok89s = nums.Where(num => num % 8 == 0 || num % 9 == 0);
+        foreach (int number in ok89s)
+        {
+            Console.WriteLine(number);
+        }
+        // 問題3.1-2
+        Console.WriteLine("問題3.1-2");
+        nums.ForEach(x =>
+        {
+            Console.WriteLine((double)x / 2);
+        });
+        // 問題3.1-3
+        Console.WriteLine("問題3.1-3");
+        var over50 = nums.Where(x => x > 50);
+        foreach (int number in over50)
+        {
+            Console.WriteLine(number);
+        }
+        // 問題3.1-4
+        Console.WriteLine("問題3.1-4");
+        var doubles = nums.Select(x => x * 2).ToList();
+        foreach (int number in doubles)
+        {
+            Console.WriteLine(number);
+        }
+        // 問題3.2
+        var cities = new List<string>
+        {
+            "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin",
+            "Canberra", "Hong Kong"
+        };
+        // 問題3.2-1
+        //Console.WriteLine("問題3.2-1");
+        //Console.WriteLine("都市名を入力してEnterを押してください");
+        //var name = Console.ReadLine();
+        //var index = cities.FindIndex(x => x == name);
+        //Console.WriteLine(index);
+        // 問題3.2-2
+        Console.WriteLine("問題3.2-2");
+        var oCount = cities.Count(x=>x.Contains("o"));
+        Console.WriteLine(oCount);
+        // 問題3.2-3
+        Console.WriteLine("問題3.2-3");
+        var containsOs = cities.Where(x => x.Contains("o"));
+        foreach (var item in containsOs)
+        {
+            Console.WriteLine(item);
+        }
+        // 問題3.2-4
+        Console.WriteLine("問題3.2-4");
+        var wordCounts = cities
+            .Where(x => x.StartsWith("B"))
+            .Select(x => x.Length);
+        foreach (var wordCount in wordCounts)
+        {
+            Console.WriteLine(wordCount);
+        }
     }
 }
