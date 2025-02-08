5b9c37e8421b43d2122c651d46a612ae20a034b9
t-mine <tomonori.mine.biz@gmail.com>
2025-02-08 21:16:11 +0900

    Chapter6

--- Chapters/Chapter6.cs
+++ Chapters/Chapter6.cs
@@ -13,9 +13,21 @@ public static class Chapter6
     public static void Do()
     {
         // 問題6.1
+        //var str1 = Console.ReadLine();
+        //var str2 = Console.ReadLine();
+        //var equal = str1.Equals(str2, StringComparison.OrdinalIgnoreCase);
+        //Console.WriteLine(equal);
+        // 問題6.2
         var str1 = Console.ReadLine();
-        var str2 = Console.ReadLine();
-        var equal = str1.Equals(str2, StringComparison.OrdinalIgnoreCase);
-        Console.WriteLine(equal);
+        if (int.TryParse(str1, out int num2))
+        {
+            var str2 = num2.ToString("N0");
+            Console.WriteLine(str2);
+        }
+        else
+        {
+            Console.WriteLine("無効な入力です。整数を入力してください。");
+        }
+
     }
 }
