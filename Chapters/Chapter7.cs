9fadbca28ce3de3d733b0ccc11fcf5571c2cc22f
t-mine <tomonori.mine.biz@gmail.com>
2025-02-12 19:48:04 +0900

    Chapter7

--- Chapters/Chapter7.cs
+++ Chapters/Chapter7.cs
@@ -12,7 +12,19 @@ public static class Chapter7
 {
     public static void Do()
     {
-        // 問題7.1
-
+        // 問題7.1.1
+        int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+        var max = numbers.Max();
+        Console.WriteLine(max);
+        // 問題7.1.2
+        var last2Numbers = numbers.Skip(numbers.Length - 2).ToArray();
+        Console.WriteLine($"Second Last: {last2Numbers[0]}, Last: {last2Numbers[1]}");
+        // 問題7.1.3
+        numbers.Select(n => n.ToString("000")).ToList().ForEach(n => Console.WriteLine(n));
+        // 問題7.1.4
+        numbers.Order().Take(3).ToList().ForEach(n => Console.WriteLine(n));
+        // 問題7.1.5
+        var result = numbers.Distinct().Count(n => n > 10);
+        Console.WriteLine($"result:{result}");
     }
 }
