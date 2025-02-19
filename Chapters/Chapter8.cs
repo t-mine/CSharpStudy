befe03fcabee36eed5074ad831bbb54274a3c959
t-mine <tomonori.mine.biz@gmail.com>
2025-02-19 19:50:21 +0900

    Chapter8

--- Chapters/Chapter8.cs
+++ Chapters/Chapter8.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpStudy.Models;
+using CSharpStudy.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,7 @@ public static class Chapter8
         var dict = new Dictionary<string, int>();
         foreach (var c in alphaStr)
         {
-            var key = c.ToString().ToLower();
+            var key = c.ToString().ToUpper();
             if (dict.ContainsKey(key))
             {
                 dict[key] = dict[key] + 1;
@@ -29,6 +30,7 @@ public static class Chapter8
                 dict.Add(key, 1);
             }
         }
+        dict = dict.OrderBy(x => x.Key).ToDictionary();
         foreach (var item in dict)
         {
             Console.WriteLine($"{item.Key}:{item.Value}");
@@ -36,12 +38,43 @@ public static class Chapter8
         // 問題8.1.1 スマートな解法
         var dict2 = str
             .Where(char.IsLetter)
-            .Select(char.ToLower)
+            .Select(char.ToUpper)
             .GroupBy(c => c)
+            .OrderBy(x => x.Key)
             .ToDictionary(g => g.Key, g => g.Count());
         foreach (var item in dict2)
         {
             Console.WriteLine($"{item.Key}:{item.Value}");
         }
+        // 問題8.1.2
+        var alphaStr3 = str.Where(c => 'a' <= c && c <= 'z');
+        var dict3 = new SortedDictionary<string, int>();
+        foreach (var c in alphaStr3)
+        {
+            var key = c.ToString().ToUpper();
+            if (dict3.ContainsKey(key))
+            {
+                dict3[key] = dict3[key] + 1;
+            }
+            else
+            {
+                dict3.Add(key, 1);
+            }
+        }
+        foreach (var item in dict3)
+        {
+            Console.WriteLine($"{item.Key}:{item.Value}");
+        }
+        // 問題8.2.1
+        var abb = new Abbreviations();
+        Console.WriteLine($"Count:{abb.Count}");
+        // 問題8.2.2
+        abb.Remove("EU");
+        Console.WriteLine($"Count after remove:{abb.Count}");
+        // 問題8.2.4
+        abb.GetDictByKeyLength(3)
+            .Select(item => $"{item.Key}={item.Value}")
+            .ToList()
+            .ForEach(s => Console.WriteLine(s));
     }
 }
---
+++ Services/Abbreviations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.Services;
+
+// 略語と対応する日本語を管理するクラス
+public class Abbreviations
+{
+    private readonly Dictionary<string, string> _dict = new();
+
+    public int Count {
+        get
+        {
+            return _dict.Count;
+        }
+    }
+
+    // コンストラクター
+    public Abbreviations()
+    {
+        var lines = File.ReadAllLines("./Files/Abbreviations.txt");
+        _dict = lines
+            .Select(line => line.Split('='))
+            .ToDictionary(x => x[0], x => x[1]);
+    }
+
+    // 要素を追加
+    public void Add(string abbr, string japanese) =>
+        _dict[abbr] = japanese;
+
+    // 要素を削除
+    public bool Remove(string abbr) =>
+        _dict.Remove(abbr);
+
+    // 省略語から対応する日本語を取り出す
+    public string? Get(string abbr) =>
+        _dict.ContainsKey(abbr) ? _dict[abbr] : null;
+
+    // 省略語から指定文字数の日本語を取り出す
+    public Dictionary<string, string> GetDictByKeyLength(int keyLength) =>
+        _dict.Where(x => x.Key.Length == keyLength).ToDictionary();
+
+    // 日本語から対応する省略語を取り出す
+    public string? ToAbbreviation(string japanese) =>
+        _dict.FirstOrDefault(x => x.Value == japanese).Key;
+
+    // 日本語の部分文字列を引数に与え、それが含まれる要素(Key,Value)をすべて取り出す
+    public IEnumerable<(string, string)> FindAll(string substring)
+    {
+        foreach (var (key, value) in _dict)
+        {
+            if (value.Contains(substring))
+            {
+                yield return (key, value);
+            }
+        }
+    }
+ }
